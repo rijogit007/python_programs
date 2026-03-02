21f36b899d40c754f8b7f7fae85df8704f53391e
Rijo007 <rijovarghese225@gmail.com>
2026-03-03 00:39:37 +0530

    first commit

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num=(int(input("enter the number to find factorial")))
+
+fact=1    
+for i in range(1,num+1,1):
+
+    fact=fact*i
+print(fact)
+
---
+++ oddoreven.py
@@ -0,0 +1,13 @@
+
+a=int(input("enter the number"))
+
+if  (a%2==0):
+    
+    print("its a even number")
+    
+else:
+    
+    print("its a odd number ")    
+    
+    
+    
